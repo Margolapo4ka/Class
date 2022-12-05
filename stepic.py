3295a87b24de597d258f8b41ea311cc6f3126499
Margo <89163432226@mail.ru>
2022-12-05 13:23:50 +0300

    Тест 1

--- stepic.py
+++ stepic.py
@@ -19,12 +19,51 @@
 #     if len(u) > 0 and u[0] == "#":
 #         continue
 #     else:
-#         print(i, end="")
-import datetime
-import random
-with open('test.txt', "r") as input:
-    with open('output.txt', "w") as output:
+#         print(i, en
 
-        for num, text in enumerate(input.readlines()):
+class SellItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-            print(f"{num+1}) {text}",  end="")
+class House(SellItem):
+    def __init__(self, name, price, material, square):
+        super().__init__(name, price)
+        self.material = material
+        self.square = square
+
+class Flat(SellItem):
+    def __init__(self, name, price, size, rooms):
+        super().__init__(name, price)
+        self.size = size
+        self.rooms = rooms
+
+class Land(SellItem):
+    def __init__(self, name, price, square):
+        super().__init__(name, price)
+        self.dims = square
+
+class Agency(list):
+    def __init__(self, name):
+        self.name = name
+        # super().__init__(name)
+
+    def add_object(self, obj):
+        super().append(obj)
+
+    def remove_object(self, obj):
+        super().remove(obj)
+
+    def get_objects(self):
+
+        return super().copy()
+ag = Agency("Рога и копыта")
+ag.add_object(Flat("квартира, 3к", 10000000, 121.5, 3))
+ag.add_object(Flat("квартира, 2к", 8000000, 74.5, 2))
+ag.add_object(Flat("квартира, 1к", 4000000, 54, 1))
+ag.add_object(House("дом, крипичный", price=35000000, material="кирпич", square=186.5))
+ag.add_object(Land("участок под застройку", 3000000, 6.74))
+for obj in ag.get_objects():
+    print(obj.name)
+
+lst_houses = [x for x in ag.get_objects() if isinstance(x, House)] # выделение списка домов
